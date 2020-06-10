99e4dbf766d4b5bdafc579c9742409adf7864814
mihainsto <mihaistoianro@gmail.com>
2020-06-11 00:21:52 +0300

    initial setup

---
+++ frontend/copyservice/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layout from "./layout/layout"
+function App() {
+  return (
+    <Layout></Layout>
+  );
+}
+
+export default App;
---
+++ frontend/copyservice/src/layout/layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./layout.scss";
+
+const Layout = () => {
+    return(
+    <div className="layout">
+        
+        <div className="title">
+            Copy Service
+        </div>
+
+        
+    </div>
+    )
+}
+
+export default Layout;
