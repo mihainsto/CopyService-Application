670a2186cec3950d3fb4dbeed5d1f4cb6f259f99
mihainsto <mihaistoianro@gmail.com>
2020-06-11 00:13:35 +0300

    Updates

---
+++ precache-manifest.b471afbe8928d6c20ef2afebe19069d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8995c2825efdfe1bccb6c3e8c192323e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d6a46a277f6c1180f7",
+    "url": "/static/css/main.143fd322.chunk.css"
+  },
+  {
+    "revision": "d6dd8057dece5fb79568",
+    "url": "/static/js/2.a8c1c841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8c1c841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d6a46a277f6c1180f7",
+    "url": "/static/js/main.b7f882e2.chunk.js"
+  },
+  {
+    "revision": "dc5b202a61a59ff30b28",
+    "url": "/static/js/runtime-main.c4c56d82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
