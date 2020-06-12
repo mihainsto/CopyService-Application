d1b359f594e3fe7b33676ae6dcc2ddb44f653e7f
Teshyx <tesileanualexandru@gmai.com>
2020-06-12 14:20:10 +0300

    IPC.h comments added
    Added comments to IPC.h file, compatible with Doxygen.

--- back-end/IPC.h
+++ back-end/IPC.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdio.h> 
 #include <string.h> 
 #include <fcntl.h> 
@@ -7,6 +6,29 @@
 #include <sys/types.h> 
 #include <unistd.h>
 
+/*  \file IPC.h
+	\breif Inter Process Communication Library
+
+	Inter-Process communication library that allows the deamon and the client to send and recive messages in a defined way.	
+
+
+*/
+
+
+/*	\struct IPCmessageToDaemon
+	\breif Struct contains the source, destination, the task and the type of job.
+	\param source Char array with len of 1024, contains the source from where it should copy.
+	\param destination Char array with len of 1024, contains the destination to where it should copy.
+	\param task Char array with len of 1024, tells the deamon what job it should accomplish:
+						-"copy" = tells the deamon to start a copy tipe job
+						-"status" = gets the status of a curent job
+						-"stop" = stops a job
+						-"suspent" = pauses / suspends a job
+						-"resume" = resumes a job
+						- \warning A wrong typed task will result in the deamon doing nothing.
+	\param jobID Integer, tells us the thread id.
+
+*/
 typedef struct IPCmessageToDaemon {
 	char source[1024];
 	char destination[1024];
@@ -15,6 +37,14 @@ typedef struct IPCmessageToDaemon {
 	int jobID;
 } IPCmessageToDaemon;
 
+
+/*	\strtuct IPCmessageToClient
+	\breif Struct contains the id of the curent job, the status and a list of other jobs.
+	\param jobList Integer arrray with len of 1024.
+	\param jobId  Integer, tells us the id / thread id.
+	\param status Float, the status of the current job. 
+
+*/
 typedef struct IPCmessageToClient {
 	int jobList[1024];
 	int jobId;
@@ -22,29 +52,39 @@ typedef struct IPCmessageToClient {
 } IPCmessageToClient;
 
 
-/*
-Send the IPCmessageToDaemon struct to the Daemon
-Use only in Clients
+/*  \fn IPC_ClientSentMessage(IPCmessageToDeamon msg)
+	\breif Send the IPCmessageToDaemon struct to the Daemon.
+	\param msg The  message sent to the deamon.
+	\returns void
+	\warning Use only in Clients!
 */
 void IPC_ClientSentMessage(IPCmessageToDaemon msg);
 
+
 /*
-Recive the IPCmessageToDaemon struct
-returns the message
-Use only in Daemon
+	\fn IPC_DaemonReciveMessage()
+	\breif Recive the IPCmessageToDaemon struct
+	\params None
+	\returns The message from Deamon
+	\warning Use only in Daemon
 */
 IPCmessageToDaemon IPC_DaemonReciveMessage();
 
-/*
-Send the IPCmessageToClient struct to the Client
-Use only in Daemon
+
+/*  \fn IPC_DaemonSentMessage(IPCmessageToClient msg)
+	\breif Send the IPCmessageToClient struct to the Client
+	\params msg The message to be sent to the Client
+	\returns void
+	\warning Use only in Daemon
 */
 void IPC_DaemonSentMessage(IPCmessageToClient msg);
 
-/*
-Recive the IPCmessageToClient struct
-returns the message
-Use only in Clients
+
+/*	\fn IPCmessageToClient IPC_ClientReciveMessage()
+	\breif Recive the IPCmessageToClient struct
+	\params None
+	\returns The message from Client
+	\Warning Use only in Clients
 */
 IPCmessageToClient IPC_ClientReciveMessage();
 
