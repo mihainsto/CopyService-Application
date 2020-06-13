76e5d9ed266962c16f0dc2d705014074210e6242
mihainsto <mihaistoianro@gmail.com>
2020-06-13 20:57:41 +0300

    add text fieds for from file and to file path

--- frontend/copyservice/src/layout/layout.jsx
+++ frontend/copyservice/src/layout/layout.jsx
@@ -6,35 +6,52 @@ import Job from "../job/job";
 const Layout = () => {
   const [jobs, setJobs] = useState({
     list: [
-      { id: "1", progress: 60, status: "working",  switchStatus:true},
-      { id: "2", progress: 100, status: "canceled", switchStatus:true },
-      { id: "3", progress: 30, status: "paused", switchStatus: true }, 
+      { id: "1", progress: 60, status: "working", switchStatus: true },
+      { id: "2", progress: 100, status: "canceled", switchStatus: true },
+      { id: "3", progress: 30, status: "paused", switchStatus: true },
     ],
   });
   const [switchStatus, setSwitchStatus] = useState("true");
+  const [pathFrom, setPathFrom] = useState("");
+  const [pathTo, setPathTo] = useState("");
   const cancelClicked = (value, id, index) => {
-    console.log(id)
+    console.log(index);
   };
   const swichClicked = (value, id, index) => {
-    setSwitchStatus(false)
-    console.log(index)
-    const oldList = jobs.list
-    const curent_status = oldList[index].switchStatus
-    if (curent_status === false){
-        oldList[index].switchStatus=true
-    } else
-    oldList[index].switchStatus=false
+    setSwitchStatus(false);
+    console.log(index);
+    const oldList = jobs.list;
+    const curent_status = oldList[index].switchStatus;
+    if (curent_status === false) {
+      oldList[index].switchStatus = true;
+    } else oldList[index].switchStatus = false;
 
-    setJobs({list: oldList})
+    setJobs({ list: oldList });
+  };
+  const inputChangedValue = (value, stateSet) =>{
+    stateSet(value.text)
   }
   return (
     <div className="layout">
       <div className="title">Copy Service</div>
       <div className="buttonContainer">
-        <button>Select Input File Path</button>
+        {/*<button>Select Input File Path</button>*/}
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Input Path"
+          value={pathFrom}
+          onChange={(value) => inputChangedValue(value, setPathFrom)}
+        />
 
-        <button>Select Output File Path</button>
-
+        {/*<button>Select Output File Path</button>*/}
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Output path"
+          value={pathTo}
+          onChange={(value) => inputChangedValue(value, setPathTo)}
+        />
         <button>Add Copy Job</button>
       </div>
       <div className="jobsContainer">
@@ -46,7 +63,7 @@ const Layout = () => {
             onClickCancel={cancelClicked}
             onSwitchClick={swichClicked}
             switchChecked={item.switchStatus}
-            index = {index}
+            index={index}
             className="job"
           ></Job>
         ))}
